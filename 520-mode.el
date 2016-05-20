c71e23182a1d35d25330c871c726946e3f394bf3
darksun <lujun9972@sina.com>
2016-05-20 13:18:02 +0800

    first commit

---
+++ 520-mode.el
@@ -0,0 +1,20 @@
+(defun 520dyd ()
+  (interactive)
+  (save-excursion
+    (when (string= "520" (buffer-substring-no-properties (- (point) 3)
+                                                         (point)))
+      (backward-char 2)
+      (insert "."))))
+
+(define-minor-mode 520-mode "我爱你多一点"
+  ;; The initial value
+  nil
+  ;; The indicator for the mode line
+  " 520"
+  :group '520-mode
+  :global t
+  (if (and (boundp '520-mode)
+           520-mode)
+      (add-hook 'post-self-insert-hook #'520dyd)
+    (remove-hook 'post-self-insert-hook #'520dyd)))
+
